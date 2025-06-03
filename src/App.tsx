a00b4062770bb2e93cf205aaf587506170fb7bac
Alisson Medeiros <alissonmelo.work@gmail.com>
2025-06-02 23:44:21 -0300

    add logo component

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
+import { Container } from './components/Container';
+import { Logo } from './components/Logo';
+
 import './styles/theme.css';
 import './styles/global.css';
-import { Container } from './components/Container';
 
 export function App() {
   return (
     <>
       <div className='container-fluid'>
         <Container>
-          <section>LOGO</section>
+          <Logo />
         </Container>
       </div>
     </>
