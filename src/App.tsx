fe75f319ff3341bc5efcf98e0a30d8cb5c5b3c46
Alisson Medeiros <alissonmelo.work@gmail.com>
2025-06-03 22:55:22 -0300

    add styles to input

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import { CountDown } from './components/CountDown';
 
 import './styles/theme.css';
 import './styles/global.css';
+import { DefaultInput } from './components/DefaultInput';
 
 export function App() {
   return (
@@ -20,6 +21,32 @@ export function App() {
 
         <Container>
           <CountDown />
+        </Container>
+
+        <Container>
+          <form className='form' action=''>
+            <div className='formRow'>
+              <DefaultInput
+                id='meuInput'
+                type='text'
+                labelText='task'
+                placeholder='Digite algo'
+              />
+            </div>
+
+            <div className='formRow'>
+              <p>Nesse ciclo descance por 5 min.</p>
+            </div>
+
+            <div className='formRow'>
+              <p>Ciclos</p>
+              <p>0 0 0 0 0 0</p>
+            </div>
+
+            <div className='formRow'>
+              <button>Enviar</button>
+            </div>
+          </form>
         </Container>
       </div>
     </>
