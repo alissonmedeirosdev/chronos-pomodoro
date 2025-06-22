496b65e95a94643f6478727a5d3e74cc7abee55f
Alisson Medeiros <alissonmelo.work@gmail.com>
2025-06-22 18:58:15 -0300

    Context API - useContext + useState (estado + contexto)

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,12 @@
+import { Home } from './pages/Home';
+import { TaskContextProvider } from './contexts/TaskContext';
 import './styles/theme.css';
 import './styles/global.css';
-import { Home } from './pages/Home';
-import type { TaskStateModel } from './models/TaskStateModel';
-import { useState } from 'react';
-import { TaskContext } from './contexts/TaskContext';
-
-const inicialState: TaskStateModel = {
-  task: [],
-  secondsRemaining: 0,
-  formattedSecondsRemaining: '00:00',
-  activeTask: null,
-  currentCycle: 0,
-  config: {
-    workTime: 25,
-    shotBreakTime: 5,
-    longBreackTime: 15,
-  },
-};
 
 export function App() {
-  const [state, setState] = useState(inicialState);
-
   return (
-    <TaskContext.Provider value={{ outraCoisa: 123 }}>
+    <TaskContextProvider>
       <Home />
-    </TaskContext.Provider>
+    </TaskContextProvider>
   );
 }
--- src/contexts/TaskContext/index.tsx
+++ src/contexts/TaskContext/index.tsx
@@ -1,5 +1,45 @@
-import { createContext } from 'react';
+import { createContext, useContext, useState } from 'react';
+import type { TaskStateModel } from '../../models/TaskStateModel';
 
-export const TaskContext = createContext({
-  chave: 'valor',
-});
+type TaskContextProps = {
+  state: TaskStateModel;
+  setState: React.Dispatch<React.SetStateAction<TaskStateModel>>;
+};
+
+type TaskContextProviderProps = {
+  children: React.ReactNode;
+};
+
+const inicialState: TaskStateModel = {
+  task: [],
+  secondsRemaining: 0,
+  formattedSecondsRemaining: '25:00',
+  activeTask: null,
+  currentCycle: 0,
+  config: {
+    workTime: 25,
+    shotBreakTime: 5,
+    longBreackTime: 15,
+  },
+};
+
+const initialContextValue = {
+  state: inicialState,
+  setState: () => {},
+};
+
+export const TaskContext = createContext<TaskContextProps>(initialContextValue);
+
+export function TaskContextProvider({ children }: TaskContextProviderProps) {
+  const [state, setState] = useState(inicialState);
+
+  return (
+    <TaskContext.Provider value={{ state, setState }}>
+      {children}
+    </TaskContext.Provider>
+  );
+}
+
+export function useTaskContext() {
+  return useContext(TaskContext);
+}
