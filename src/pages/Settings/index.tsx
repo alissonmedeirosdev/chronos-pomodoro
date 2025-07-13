51715fe4d8218f0123d188075d5967e4f250cfd2
Alisson Medeiros <alissonmelo.work@gmail.com>
2025-07-13 23:44:47 +0000

    Validando os campos do formulário antes de salvar os dados

--- src/pages/Settings/index.tsx
+++ src/pages/Settings/index.tsx
@@ -5,15 +5,20 @@ import { DefaultInput } from '../../components/DefaultInput';
 import { Heading } from '../../components/Heading';
 import { MainTemplate } from '../../templetes/MainTemplate';
 import { useRef } from 'react';
+import { useTaskContext } from '../../contexts/TaskContext/useTaskContext';
+import { showMessage } from '../../adapters/showMessage';
 
 /*
 
   Para pegar um valor enviado de um formulário para um userRef, vocẽ coloca
   no input o ref={}
 
+  E para inputs que o valor não é controlado não é value e sim defaultValue
+
 */
 
 export function Settings() {
+  const { state } = useTaskContext();
   const workTimeInput = useRef<HTMLInputElement | null>(null);
   const shortBreakTimeInput = useRef<HTMLInputElement | null>(null);
   const longBreakTimeInput = useRef<HTMLInputElement | null>(null);
@@ -21,11 +26,36 @@ export function Settings() {
   function handleSaveSattings(event: React.FormEvent<HTMLFormElement>) {
     event.preventDefault();
 
-    const worktime = workTimeInput.current?.value;
-    const shortBreakTime = shortBreakTimeInput.current?.value;
-    const longBreakTime = longBreakTimeInput.current?.value;
+    showMessage.dismiss();
 
-    console.log(worktime, shortBreakTime, longBreakTime);
+    const formErrors: string[] = [];
+    const worktime = Number(workTimeInput.current?.value);
+    const shortBreakTime = Number(shortBreakTimeInput.current?.value);
+    const longBreakTime = Number(longBreakTimeInput.current?.value);
+
+    if (isNaN(worktime) || isNaN(shortBreakTime) || isNaN(longBreakTime)) {
+      formErrors.push('Digite apenas números para TODOS os campos');
+    }
+
+    if (worktime < 1 || worktime > 99) {
+      formErrors.push('Digite valores entre 1 a 99 para foco');
+    }
+
+    if (shortBreakTime < 1 || shortBreakTime > 30) {
+      formErrors.push('Digite valores entre 1 a 30 para descanso curto');
+    }
+
+    if (longBreakTime < 1 || longBreakTime > 60) {
+      formErrors.push('Digite valores entre 1 a 60 para descanso longo');
+    }
+
+    if (formErrors.length > 0) {
+      formErrors.forEach(error => {
+        showMessage.error(error);
+      });
+      return;
+    }
+    console.log('SALVAR');
   }
 
   return (
@@ -42,7 +72,13 @@ export function Settings() {
       <Container>
         <form onSubmit={handleSaveSattings} action='' className='form'>
           <div className='formRow'>
-            <DefaultInput id='worktime' labelText='Foco' ref={workTimeInput} />
+            <DefaultInput
+              id='worktime'
+              labelText='Foco'
+              ref={workTimeInput}
+              defaultValue={state.config.workTime}
+              type='number'
+            />
           </div>
 
           <div className='formRow'>
@@ -50,6 +86,8 @@ export function Settings() {
               id='shortBreakTime'
               labelText='Descanso curto'
               ref={shortBreakTimeInput}
+              defaultValue={state.config.shortBreakTime}
+              type='number'
             />
           </div>
 
@@ -58,6 +96,8 @@ export function Settings() {
               id='longBreakTime'
               labelText='Descanso longo'
               ref={longBreakTimeInput}
+              defaultValue={state.config.longBreakTime}
+              type='number'
             />
           </div>
 
