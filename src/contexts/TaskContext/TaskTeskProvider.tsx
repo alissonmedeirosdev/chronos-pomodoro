5711673220df3d7c3305de8d3406a671afa0147f
Alisson Medeiros <alissonmelo.work@gmail.com>
2025-07-13 11:46:26 +0000

    useReducer e lazy initializer - salvando e recuperando o estado do localStorage

--- src/contexts/TaskContext/TaskTeskProvider.tsx
+++ src/contexts/TaskContext/TaskTeskProvider.tsx
@@ -5,6 +5,7 @@ import { taskReducer } from './taskReducer';
 import { TimerWorkManager } from '../../workers/TimerWorkerManager';
 import { TaskActionTypes } from './taskActions';
 import { loadBeep } from '../../utils/loadBeep';
+import type { TaskStateModel } from '../../models/TaskStateModel';
 
 type TaskContextProviderProps = {
   children: React.ReactNode;
@@ -12,7 +13,21 @@ type TaskContextProviderProps = {
 
 export function TaskContextProvider({ children }: TaskContextProviderProps) {
   // const [state, setState] = useState(initialTaskState);
-  const [state, dispatch] = useReducer(taskReducer, initialTaskState);
+  const [state, dispatch] = useReducer(taskReducer, initialTaskState, () => {
+    // incialição preguiçosa
+    const storageState = localStorage.getItem('state');
+
+    if (storageState === null) return initialTaskState;
+
+    const parsedStorageState = JSON.parse(storageState) as TaskStateModel;
+
+    return {
+      ...parsedStorageState,
+      activeTask: null,
+      secondsRemaining: 0,
+      formattedSecondsRemaining: '00:00',
+    };
+  });
   const playBeepRef = useRef<ReturnType<typeof loadBeep> | null>(null);
 
   const worker = TimerWorkManager.getInstance();
@@ -38,6 +53,9 @@ export function TaskContextProvider({ children }: TaskContextProviderProps) {
   });
 
   useEffect(() => {
+    // Estado mudou
+    localStorage.setItem('state', JSON.stringify(state));
+
     if (!state.activeTask) {
       worker.terminate();
     }
